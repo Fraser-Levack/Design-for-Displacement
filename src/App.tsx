35a29bfea6db543c81dc9b33230e9d67a3cf812a
Fraser W Levack <2777042L@student.gla.ac.uk>
2024-10-25 12:51:16 +0100

    fix - better layout. removed WorldMap Zoom.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import MainHeading from './MainHeading.tsx';
+//import MainHeading from './MainHeading.tsx';
 import WorldMap from "./WorldMap.tsx";
 import './css/App.css';
 
 function App() {
   return (
     <div>
-        <MainHeading>Design for Displacement</MainHeading>
-        <MainHeading isSubheading={true}>A project by Dundee Uni</MainHeading>
         <WorldMap fill="#333" stroke="#000"/>
     </div>
   );
--- src/WorldMap.tsx
+++ src/WorldMap.tsx
@@ -1,6 +1,7 @@
-import { ComposableMap, Geographies, Geography , ZoomableGroup} from "react-simple-maps"
-import { useState } from "react";
+import { ComposableMap, Geographies, Geography} from "react-simple-maps"
+//import { useState } from "react";
 import "./css/WorldMap.css"
+import MainHeading from "./MainHeading.tsx";
 
 interface GeographyType {
     rsmKey: string;
@@ -14,32 +15,18 @@ interface Props {
 
 function WorldMap ({fill, stroke} : Props) {
 
-    const [position, setPosition] = useState({ coordinates: [50, 25], zoom: 1 , translateExtent: [[0, 0], [0, 0]]});
-
     // high definition map
-    const geoUrl =
-        "https://cdn.jsdelivr.net/npm/world-atlas@2/countries-50m.json"
+    const geoUrl = "https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json"
+
+        //"https://cdn.jsdelivr.net/npm/world-atlas@2/countries-50m.json"
 
     // low def on this link
-    // https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json
-
-    function handleZoom() {
-        const coords = [50,25];
-        const zoomMultiplier = 2;
-        if (position.zoom > 1) {
-            return setPosition({ coordinates: coords, zoom: 1 , translateExtent: [[0, 0], [0, 0]]});
-        }
-        const zoom = position.zoom * zoomMultiplier;
-        setPosition({ coordinates: coords, zoom: zoom, translateExtent: [[0, 0], [0, 0]]});
-    }
 
     return (
-        <ComposableMap className="world-map" onClick={handleZoom}>
-          <ZoomableGroup zoom={position.zoom}
-                         center={position.coordinates}
-                         disablePanning={true}
-                         transitionDuration={800}
-          >
+        <>
+        <MainHeading>Design for Displacement</MainHeading>
+        <MainHeading isSubheading={true}>A project by Dundee Uni</MainHeading>
+        <ComposableMap className="world-map" viewBox="0 0 850 600">
 
           <Geographies geography={geoUrl}>
               {({ geographies }: { geographies: GeographyType[] }) =>
@@ -52,8 +39,8 @@ function WorldMap ({fill, stroke} : Props) {
                   ))
               }
           </Geographies>
-          </ZoomableGroup>
         </ComposableMap>
+        </>
     );
 }
 
