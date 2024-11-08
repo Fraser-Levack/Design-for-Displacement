355f2e000e40972e619100b9537f0914678590be
Fraser W Levack <2777042L@student.gla.ac.uk>
2024-11-08 21:17:31 +0000

    fix - undo past changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/Design-for-Displacement/',
   build: {
     outDir: 'build',
   },
