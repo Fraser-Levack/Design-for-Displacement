3b21fa2b4ca71b0271b2506552da71b9f8b43030
Fraser W Levack <2777042L@student.gla.ac.uk>
2024-11-09 19:22:48 +0000

    feat - generalised InfoBlock.tsx + simple styling.

--- src/App.tsx
+++ src/App.tsx
@@ -20,31 +20,20 @@ function App() {
     </div>
     <div className="page-content">
     <div className="map-content">
-        <WorldGlobe color={'#7782b4'}/>
+        <WorldGlobe color={'#bfbfbf'}/>
     </div>
     < StickyBar />
 
-      <InfoBlock title={"Psychosocial Impact"}>
+      <InfoBlock title={"Psychosocial Impact"} span={[40, 50]} columns={2}>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur.
-          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur.
-          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Gustaria
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
         </p>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur.
-          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
         </p>
       </InfoBlock>
 
--- src/components/InfoBlock.tsx
+++ src/components/InfoBlock.tsx
@@ -5,18 +5,28 @@ import React from "react";
 interface Props {
     children: React.ReactNode;
     title: string;
+    span?: number[];
+    columns?: number;
 }
 
-function InfoBlock({children, title}: Props) {
+function InfoBlock({children, title, span, columns = 1}: Props) {
+    const infoTextStyle = {
+        display: 'grid',
+        gridTemplateColumns: columns === 2 ? '1fr 1fr' : '1fr',
+        gap: '1rem'
+    };
+
     return (
         <div className={"info-block"}>
-            <div className={"info-text-section"}>
+
+            <div className={"info-text-section"} style={{width: span ? `${span[0]}%` : '50%'}}>
                 <h2>{title}</h2>
-                <p>
+                <div className={"info-text"} style={infoTextStyle}>
                     {children}
-                </p>
+                </div>
             </div>
-            <div className={"info-image-section"}>
+
+            <div className={"info-image-section"} style={{width: span ? `${span[1]}%` : '40%'}}>
 
             </div>
         </div>
