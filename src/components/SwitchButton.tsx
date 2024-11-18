9e0f110d2f050e331025b81ee1abc8bf26e73c85
Fraser W Levack <2777042L@student.gla.ac.uk>
2024-11-18 13:49:33 +0000

    feat - more aesthetic switch button.

--- src/components/SwitchButton.tsx
+++ src/components/SwitchButton.tsx
@@ -7,10 +7,13 @@ interface Props {
 
 function SwitchButton ({ setMapSwitch, mapSwitch }: Props) {
   return (
-    <div className="switch-button">
-      <button onClick={() => setMapSwitch(!mapSwitch)}>
-        {mapSwitch ? "Switch to Globe" : "Switch to Map"}
-      </button>
+    <div className={"switch-content"}>
+      <div className={"switch-button"} onClick={() => setMapSwitch(!mapSwitch)}>
+          <div className={`switch-circle ${mapSwitch ? "active" : ""}`}/>
+      </div>
+        <div className={"switch-text"}>
+            {mapSwitch ? "Switch to Globe" : "Switch to Map"}
+      </div>
     </div>
   );
 }
