e2612753b1452ce99b96d6caf75741f65dee17d6
Fraser W Levack <2777042L@student.gla.ac.uk>
2024-11-25 16:58:39 +0000

    chore - added PDME testing images under meso

--- src/components/Meso.tsx
+++ src/components/Meso.tsx
@@ -3,7 +3,7 @@ import InfoBlock from "./content/InfoBlock.tsx";
 function Meso () {
   return (
     <div className={'meso-content'}>
-        <InfoBlock title={"Psychosocial Impact Meso"} span={[40, 50]}>
+        <InfoBlock title={"Introduction"} span={[40, 50]} columns={2} id={"Introduction"} image_src={"sample_images/PDME 1 webp.webp"}>
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Gustaria
@@ -16,6 +16,31 @@ function Meso () {
             </p>
         </InfoBlock>
 
+        <InfoBlock title={"Politics"} span={[40, 50]} columns={2} id={"Politics"} image_src={"sample_images/PDME 1 jpg.jpg"}>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Gustaria
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <p>
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+        </InfoBlock>
+
+        <InfoBlock title={"Economics"} span={[40, 50]} columns={2} id={"Economics"} image_src={"sample_images/PDME 1 pdf.png"}>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Gustaria
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <p>
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+        </InfoBlock>
     </div>
   );
 }
