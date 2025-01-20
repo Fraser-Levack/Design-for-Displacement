10989f487a900ec46e52fed4679d3919ef3f905e
Fraser W Levack <2777042L@student.gla.ac.uk>
2025-01-20 17:12:53 +0000

    chore - admin initial styling

--- src/components/Admin.tsx
+++ src/components/Admin.tsx
@@ -1,12 +1,29 @@
-import InputBlock from "./admin/InputBlock.tsx";
+import { Link } from 'react-router-dom';
+import '../css/Admin.css';
 
 function Admin ()   {
     return (
         <div>
-            <h1>Admin</h1>
-            <InputBlock />
+            <h1 className={"admin-header"}>Design For Displacement Admin</h1>
+            <div className={"sections"}>
+                <section className={"macro-section"}>
+                    <Link to="/admin/macro" className={"custom-link"}>
+                        <h2>Macro</h2>
+                    </Link>
+                </section>
+                <section className={"meso-section"} >
+                    <Link to="/admin/meso" className={"custom-link"}>
+                        <h2>Meso</h2>
+                    </Link>
+                </section>
+                <section className={"micro-section"}>
+                    <Link to="/admin/micro" className={"custom-link"}>
+                        <h2>Micro</h2>
+                    </Link>
+                </section>
+            </div>
         </div>
-    )
+)
 }
 
 export default Admin;
