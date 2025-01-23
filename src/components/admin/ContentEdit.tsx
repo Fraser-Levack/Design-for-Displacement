3477797292cf1e24f8baca756b1f16437f0f1d28
Fraser W Levack <2777042L@student.gla.ac.uk>
2025-01-23 14:27:11 +0000

    feat - delete prompt added

--- src/components/admin/ContentEdit.tsx
+++ src/components/admin/ContentEdit.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import '../../css/admin/ContentEdit.css';
 
 interface props {
@@ -8,17 +8,52 @@ interface props {
 
 function ContentEdit ({blockId, blockContent}: props) {
     const [isOpen, setIsOpen] = useState<boolean>(false);
+    const [isDeleteBox, setIsDeleteBox] = useState<boolean>(false);
+    const deleteBoxRef = useRef<HTMLDivElement>(null);
 
     function handleOpen() {
         setIsOpen(!isOpen);
     }
 
+    function handleDeleteBoxShow() {
+        setIsDeleteBox(!isDeleteBox);
+    }
+
+    useEffect(() => {
+        function handleClickOutside(event: MouseEvent) {
+            if (deleteBoxRef.current && !deleteBoxRef.current.contains(event.target as Node)) {
+                setIsDeleteBox(false);
+            }
+        }
+
+        if (isDeleteBox) {
+            document.addEventListener('mousedown', handleClickOutside);
+        } else {
+            document.removeEventListener('mousedown', handleClickOutside);
+        }
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [isDeleteBox]);
+
     return (
         <div className={'content-edit'}>
+            <section ref={deleteBoxRef} className={`delete-box ${!isDeleteBox ? 'null-del-box' : ''}`}>
+                <h2> Are you sure you would like to delete block {blockId}?</h2>
+                <div className={'delete-options'}>
+                    <h3>Yes</h3>
+                    <h3 onClick={handleDeleteBoxShow}>No</h3>
+                </div>
+            </section>
             <section className={'top-section-options'}>
                 <h3>{blockId}</h3>
-                <img src='/icons/chevron-right.svg' alt={'options'}
-                     className={isOpen ? 'chevron-open' : 'chevron-closed'} onClick={handleOpen}/>
+                <div className={'icon-options'}>
+                    <img src='/icons/trash.svg' alt={'delete'} className={'delete-icon'} onClick={handleDeleteBoxShow}/>
+
+                    <img src='/icons/chevron-right.svg' alt={'options'}
+                         className={isOpen ? 'chevron-open' : 'chevron-closed'} onClick={handleOpen}/>
+                </div>
             </section>
             <section className={`lower-section-content ${!isOpen ? 'closed' : ''}`}>
                 <p>{blockContent}</p>
